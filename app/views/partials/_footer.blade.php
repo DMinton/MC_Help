e7c26401987b4f57a773af5b9ee38357f63136e7
unknown <davidpaulminton@gmail.com>
2014-02-20 17:17:12 -0500

    changed footer

--- app/views/partials/_footer.blade.php
+++ app/views/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <div class="navbar navbar-default navbar-fixed-bottom">
-	<div class="nav navbar-nav navbar-right">
+	<div class="nav navbar-nav">
 		<p class="navbar-text"><small><em>Designed By: David Minton</em></small></p>
 	</div>
 </div>
