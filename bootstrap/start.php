2b70306c0b28211df0a1bcac9f84c57278252f0a
David <davidpaulminton@gmail.com>
2014-03-05 14:37:19 -0500

    removed asset pipeline

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
     'local' => array('david'),
     'local' => array('David-HP'),
-  ));
+));
 
 /*
 |--------------------------------------------------------------------------
