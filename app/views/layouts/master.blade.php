8f12a8329e53da6c1856bd8f78a1530a2a3b2dd0
David <davidpaulminton@gmail.com>
2014-03-05 14:40:55 -0500

    live server push [trigger:composer:update]

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -3,11 +3,14 @@
 	<head>
 		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>MC_Help</title>
+
 		<link href="{{ asset('assets/stylesheets/bootstrap-theme.min.css') }}" rel="stylesheet">
 		<link href="{{ asset('assets/stylesheets/bootstrap.min.css') }}" rel="stylesheet">
 		<link href="{{ asset('assets/stylesheets/stylesheet.css') }}" rel="stylesheet">
+		
     	<script src="{{ asset('assets/javascripts/1.11.0.min.js') }}" ></script>
     	<script src="{{ asset('assets/javascripts/bootstrap.min.js') }}" ></script>
+
 	</head>
 	<body>
 		<div class="container">
