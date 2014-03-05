2d5b7a192abd2274214e8b2db0a66d9b5c0c95fa
David <davidpaulminton@gmail.com>
2014-03-05 13:13:26 -0500

    edit

--- app/library/interfaces/LastModelInterface.php
+++ app/library/interfaces/LastModelInterface.php
@@ -4,5 +4,4 @@ interface LastModelInterface {
 	public function findLast($parentpost_id);
     public function getLastPost($cate_id);
     public function createLast($post, $cate_id);
-    public function getLastCategory($parentpost_id, $cate_id);
 }
