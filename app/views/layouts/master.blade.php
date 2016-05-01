f76f1ae48863f0a4a19c6e062dfa0c1897171f45
David Minton <dminton@dminton.local>
2016-04-30 18:25:05 -0700

    use https

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -4,12 +4,12 @@
 		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>MC_Help</title>
 
-		<link href="{{ asset('assets/stylesheets/bootstrap-theme.min.css') }}" rel="stylesheet">
-		<link href="{{ asset('assets/stylesheets/bootstrap.min.css') }}" rel="stylesheet">
-		<link href="{{ asset('assets/stylesheets/stylesheet.css') }}" rel="stylesheet">
-		
-    	<script src="{{ asset('assets/javascripts/1.11.0.min.js') }}" ></script>
-    	<script src="{{ asset('assets/javascripts/bootstrap.min.js') }}" ></script>
+		<link href="{{ secure_asset('assets/stylesheets/bootstrap-theme.min.css') }}" rel="stylesheet">
+		<link href="{{ secure_asset('assets/stylesheets/bootstrap.min.css') }}" rel="stylesheet">
+		<link href="{{ secure_asset('assets/stylesheets/stylesheet.css') }}" rel="stylesheet">
+
+    	<script src="{{ secure_asset('assets/javascripts/1.11.0.min.js') }}" ></script>
+    	<script src="{{ secure_asset('assets/javascripts/bootstrap.min.js') }}" ></script>
 
 	</head>
 	<body>
