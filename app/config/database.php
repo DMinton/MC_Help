935cefc243dd8889cf8c1b03666feaa2054c60a6
David Minton <dminton@dminton.local>
2016-04-30 18:30:43 -0700

    use https

--- app/config/database.php
+++ app/config/database.php
@@ -54,7 +54,7 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => getenv('CLEARDB_DATABASE_URL'],
+			'host'      => getenv('CLEARDB_DATABASE_URL'),
 			'database'  => getenv('DB_NAME'),
 			'username'  => getenv('DB_USER'),
 			'password'  => getenv('DB_PASSWORD'),
